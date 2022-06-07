2ce1fea74a81b0891183fa45e1eaf9ee8c09bab7
ryan binas <born010code@gmail.com>
2022-06-07 10:58:24 +0800

    feature/store-schedules

---
+++ src/handlers/store-schedules.js
@@ -0,0 +1,222 @@
+const serverless = require('serverless-http');
+const body_parser = require('body-parser');
+const express = require('express');
+const app = express();
+app.use(body_parser.json({ strict: false }));
+app.use(body_parser.urlencoded({ extended: true }));
+
+// Middlewares
+var verifyAdminToken = require('../middlewares/verifyAdminToken');
+
+// Helper declarations
+const API_RESPONSE = require('../helpers/api-response');
+const QUERY_HELPER = require('../helpers/query-helper');
+const TEXT_HELPER = require('../helpers/text');
+
+// Model declarations
+const STORE_SCHEDULE_MODEL = require("../models/store-schedule");
+const STORE_MODEL = require("../models/store");
+
+// Validator declarations
+const STORE_CONFIG_VALIDATOR = require("../validators/store-schedule-validator");
+
+// Lib declarations
+const moment = require('moment');
+
+// GET ONE RECORD
+app.get('/admin/store-schedules/:id', verifyAdminToken, async (req, res, next) => {
+
+    let params = await QUERY_HELPER.prepare(req);
+    params.conditions = {
+        "id": parseInt(req.params.id || 0)
+    }
+
+    try {
+        let results = await STORE_SCHEDULE_MODEL.getOne(params);
+        
+        API_RESPONSE.send(res, {
+            'status': 200,
+            'success': true,
+            'message': 'Data successfully retrieved.',
+            'data': results,
+        });
+    }  catch( error ) {
+        API_RESPONSE.send(res, {
+            'status': error.code ? error.code : 500,
+            'success': false,
+            'message': error.message,
+        });
+    }
+});
+
+// GET RECORDS
+app.get('/admin/store-schedules', verifyAdminToken, async (req, res, next) => {
+    let params = await QUERY_HELPER.prepare(req);
+
+    try {
+        let results = await STORE_SCHEDULE_MODEL.get(params);
+        
+        API_RESPONSE.send(res, {
+            'status': 200,
+            'success': true,
+            'message': 'Data successfully retrieved.',
+            'data': results,
+        })
+    }  catch( error ) {
+        API_RESPONSE.send(res, {
+            'status': error.code ? error.code : 500,
+            'success': false,
+            'message': error.message,
+        });
+    }
+});
+
+
+// UPDATE RECORD
+app.put('/admin/store-schedules/:id', verifyAdminToken, async (req, res, next) => {
+
+    let params = {};
+    params.body = req.body;
+    params.currentUser = req.currentUser;
+    let store_id = parseInt( req.params.id || 0);
+    
+    // Validataion
+    params.conditions = {
+        "id": store_id
+    }
+
+    if ( !params.conditions.id ) {
+        API_RESPONSE.send(res, {
+            'status': 404,
+            'success': false,
+            'message': 'Unable to process request.',
+        });
+        return;
+    }
+    
+    try {
+        // Preparations
+        params.setSql = await STORE_SCHEDULE_MODEL.prepareUpdate(params);
+        
+        // Perform Query
+        let results = await STORE_SCHEDULE_MODEL.update(params);
+        
+        API_RESPONSE.send(res, {
+            'status': 200,
+            'success': true,
+            'message': 'Record successfully updated.',
+            'data': results,
+        });
+    }  catch( error ) {
+        API_RESPONSE.send(res, {
+            'status': error.code ? error.code : 500,
+            'success': false,
+            'message': error.message,
+        });
+    }
+});
+
+
+// CREATE RECORD
+app.post('/admin/store-schedules', verifyAdminToken, async (req, res, next) => {
+
+    let params = {}
+    params.body = req.body;
+    params.currentUser = req.currentUser;
+    // Validataion
+    try {
+        let validator = await STORE_CONFIG_VALIDATOR.validate(req.body);
+        if ( !validator.valid ) {
+            API_RESPONSE.send(res, {
+                'status': 422,
+                'success': false,
+                'errors': validator.errors,
+                'message': 'Invalid data.',
+            });
+            return;
+        } 
+    } catch( error ) {
+        API_RESPONSE.send(res, {
+            'status': error.code ? error.code : 500,
+            'success': false,
+            'message': error.message,
+        });
+        return;
+    }
+
+    // Now, we need to check if store exist or not
+    let storeParams = await QUERY_HELPER.prepare(req);
+
+    // Find soms_acccount
+    storeParams.conditions = {
+        "id": ( req.body.store_id || null )
+    }
+    let store = null;
+
+    try {
+        store = await STORE_MODEL.getOne(storeParams);
+    }  catch( error ) {
+        API_RESPONSE.send(res, {
+            'status': error.code ? error.code : 500,
+            'success': false,
+            'message': error.message,
+        });
+        return;
+    }
+
+    // Let's go
+    try {
+        // Preparations
+        params.insertSql = await STORE_SCHEDULE_MODEL.prepareSave(params);
+        
+        // Perform Query
+        let results = await STORE_SCHEDULE_MODEL.save(params);
+        
+        API_RESPONSE.send(res, {
+            'status': 201,
+            'success': true,
+            'message': 'Record successfully created.',
+            'data': results,
+        });
+    }  catch( error ) {
+        API_RESPONSE.send(res, {
+            'status': error.code ? error.code : 500,
+            'success': false,
+            'message': error.message,
+        });
+    }
+});
+
+
+// DELETE RECORD
+app.delete('/admin/store-schedules/:id', verifyAdminToken, async (req, res, next) => {
+
+    let params = {};
+    params.currentUser = req.currentUser;
+    params.id = req.params.id || 0;
+    // Validataion
+    
+    try {
+         // Preparations
+         params.deleteSql = await STORE_SCHEDULE_MODEL.prepareDelete(params);
+
+        // Perform Query
+        let results = await STORE_SCHEDULE_MODEL.delete(params);
+        
+        API_RESPONSE.send(res, {
+            'status': 200,
+            'success': true,
+            'message': 'Record successfully deleted.',
+            'data': results,
+        });
+    }  catch( error ) {
+        API_RESPONSE.send(res, {
+            'status': error.code ? error.code : 500,
+            'success': false,
+            'message': error.message,
+        });
+    }
+});
+
+
+module.exports.handler = serverless(app);
---
+++ src/validators/store-schedule-validator.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    validate: async function(payload) {
+        let Validator = require('jsonschema').Validator;
+        let v = new Validator();
+        
+        let schema = require('../schemas/store-schedule-schema.json');
+        return v.validate(payload, schema);
+    },
+}
